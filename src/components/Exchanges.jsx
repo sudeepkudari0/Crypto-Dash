a67fa6c1c02e28b2d40c3f396dbcc89cf0e3532a
Sudeep kudari <141812041+sudeepkudari0@users.noreply.github.com>
2023-09-29 18:33:01 +0000

    Changes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,43 +1,91 @@
-import {React,useEffect,useState} from 'react'
-import { HStack} from '@chakra-ui/react'
-import axios from 'axios'
-import ExchangeCard from '../components/ExchangeCard'
-// import {server} from '../components/Home' check for error here ... its not detecting the import
-import { Container } from '@chakra-ui/react'
-import Loader from '../components/Loader'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import { server } from "../main";
+import {
+  Container,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+// import ErrorComponent from "./ErrorComponent";
 
 const Exchanges = () => {
-  const [Exchanges, setExchanges] = useState([]);
+  const [exchanges, setExchanges] = useState([]);
   const [loading, setLoading] = useState(true);
-    useEffect(() => {
-      const fetchExchanges = async()=>{
-      const {data} = await axios.get('https://api.coingecko.com/api/v3/exchanges?per_page=100');
+  // const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      try {
+        const { data } = await axios.get(`https://api.coingecko.com/api/v3/exchanges`);
         setExchanges(data);
-        console.log(data)
+        setLoading(false);
+      } catch (error) {
+        // setError(true);
         setLoading(false);
       }
-      
-      fetchExchanges();
-    }
-    , [])
-    
-    return (
-      <Container maxW={'xl'}>
-        {loading?<Loader/>:<>
-        <HStack wrap={"wrap"}>
-      {
-        Exchanges.map((i)=>(
-        <ExchangeCard key = {i.id} name = {i.name} img = {i.image} rank = {i.trust_score_rank} url = {i.url}/>       
-         ))
-      }
+    };
+    fetchExchanges();
+  }, []);
 
-        </HStack>
-        </>}
+  // if (error)
+  //   return <ErrorComponent message={"Error While Fetching Exchanges"} />;
 
-      </Container>
-  )
-}
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
 
+const ExchangeCard = ({ name, img, rank, url }) => (
+  <a href={url} target={"blank"}>
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        w={"10"}
+        h={"10"}
+        objectFit={"contain"}
+        alt={"Exchange"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
 
+      <Text noOfLines={1}>{name}</Text>
+    </VStack>
+  </a>
+);
 
-export default Exchanges
+export default Exchanges;
